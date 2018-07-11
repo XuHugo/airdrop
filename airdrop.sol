c5a741fded09d3280d8b5d5e02a7915f59f9862e
XuHugo <xq-310@163.com>
2018-07-11 14:59:45 +0800

    Update airdrop.sol

--- airdrop.sol
+++ airdrop.sol
@@ -1,40 +1,22 @@
 pragma solidity ^0.4.18;
  
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
 contract Ownable {
   address public owner;
  
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
   function Ownable() public {
     owner = msg.sender;
   }
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
 }
 
-interface IERC20 {
-  //function totalSupply() public constant returns (uint256 );
+interface Token {
   function balanceOf(address _owner) public constant returns (uint256 );
   function transfer(address _to, uint256 _value) public ;
-  //function decimals() public constant returns (uint8 decimals);
-  //function transferFrom(address _from, address _to, uint256 _value) public returns (bool );
-  //function approve(address _spender, uint256 _value) public returns (bool );
-  //function allowance(address _owner, address _spender) public constant returns (uint256 );
-  //event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  //event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
 }
 
 contract Airdropper is Ownable {
@@ -42,7 +24,7 @@ contract Airdropper is Ownable {
     function batchTransfer(address[] _recipients, uint _values, address _tokenAddress) onlyOwner public returns (bool) {
         require(_recipients.length > 0);
 
-        IERC20 token = IERC20(_tokenAddress);
+        Token token = Token(_tokenAddress);
         
         for(uint j = 0; j < _recipients.length; j++){
             token.transfer(_recipients[j], value);
@@ -52,7 +34,7 @@ contract Airdropper is Ownable {
     }
  
      function withdrawalToken(address _tokenAddress) onlyOwner public { 
-        IERC20 token = IERC20(_tokenAddress);
+        Token token = Token(_tokenAddress);
         token.transfer(owner, token.balanceOf(this));
     }
 
